0e59c7c67fb4ed7ef3de63f0f4ddbe82a7662bda
“Best <bestnyah7@gmail.com>
2023-04-06 21:32:44 +0100

    added dbt to depl

--- flows/dbt_flow.py
+++ flows/dbt_flow.py
@@ -2,7 +2,7 @@ from prefect import flow
 from prefect_dbt.cloud import DbtCloudCredentials
 from prefect_dbt.cloud.jobs import trigger_dbt_cloud_job_run_and_wait_for_completion
 
-
+# main flow
 @flow
 def run_dbt_cloud_job_flow():
       dbt_cloud_credentials = DbtCloudCredentials.load("dbt-cloud-cred")
